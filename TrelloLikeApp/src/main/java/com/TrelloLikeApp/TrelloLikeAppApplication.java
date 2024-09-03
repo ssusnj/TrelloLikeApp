4b741c3359ce3399667ec4b4a64d266c9d70cfc4
zusassr <sara.susnjara.ext@ericsson.com>
2024-09-03 20:00:40 +0200

    Add initial project files

---
+++ TrelloLikeApp/src/main/java/com/TrelloLikeApp/TrelloLikeAppApplication.java
@@ -0,0 +1,13 @@
+package com.TrelloLikeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloLikeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloLikeAppApplication.class, args);
+	}
+
+}
---
+++ TrelloLikeApp/src/test/java/com/TrelloLikeApp/TrelloLikeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrelloLikeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloLikeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
